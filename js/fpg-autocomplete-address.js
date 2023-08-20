1bed160c529d5fd4f4a87b817475c0d72d15396d
Fernando Paglacci <fernandopaglacci@gmail.com>
2023-08-19 23:28:27 -0300

    Update fpg-autocomplete-address.js
    - Adicionado tratamento de erros para a chamada AJAX no arquivo JavaScript
    - Adicionado validação dos dados recebidos da API no arquivo JavaScript
    - Adicionado uso de seletores mais específicos para selecionar os elementos do formulário no arquivo JavaScript
    - Adicionado preenchimento automático dos campos de endereço assim que o usuário terminar de digitar o CEP completo no arquivo JavaScript
    - Adicionado parâmetro de consulta exclusivo à URL da chamada AJAX para evitar cache no arquivo JavaScript
    - Adicionado uso do recurso de rastreamento de atualização do WooCommerce para mostrar ao cliente que o endereço está sendo carregado no arquivo JavaScript

--- js/fpg-autocomplete-address.js
+++ js/fpg-autocomplete-address.js
@@ -1,49 +1,47 @@
-jQuery(document).ready(function($) {
-
+jQuery(document).ready(function ($) {
     // Função para preencher automaticamente o endereço com base no CEP
-    function autocompleteAddressByCEP(cep) {
+    function autocompleteAddressByCEP(cep, prefix) {
+        // Mostra o ícone de carregamento
+        $(`#${prefix}_postcode_field`).block({
+            message: null,
+            overlayCSS: {
+                background: '#fff',
+                opacity: 0.6
+            }
+        });
+
         $.ajax({
-            url: 'https://brasilapi.com.br/api/cep/v2/' + cep,
+            url: 'https://brasilapi.com.br/api/cep/v2/' + cep + '?_=' + new Date().getTime(),
             method: 'GET',
             dataType: 'json',
-            success: function(data) {
-                if (data.state && data.city && data.neighborhood && data.street) {
-                    // Preencha os campos de cobrança (billing) com os dados obtidos da API
-                    $('#billing_state').val(data.state).change();
-                    setTimeout(function() {
-                        $('#billing_state').trigger('chosen:updated');
+            success: function (data) {
+                if (data && data.state && data.city && data.neighborhood && data.street) {
+                    // Preenche os campos de endereço com os dados obtidos da API
+                    $(`#${prefix}_state`).val(data.state).change();
+                    setTimeout(function () {
+                        $(`#${prefix}_state`).trigger('chosen:updated');
                     }, 100);
-                    $('#billing_city').val(data.city);
-                    $('#billing_neighborhood').val(data.neighborhood);
-                    $('#billing_address_1').val(data.street);
-
-                    // Preencha os campos de envio (shipping) com os dados obtidos da API
-                    $('#shipping_state').val(data.state).change();
-                    setTimeout(function() {
-                        $('#shipping_state').trigger('chosen:updated');
-                    }, 100);
-                    $('#shipping_city').val(data.city);
-                    $('#shipping_neighborhood').val(data.neighborhood);
-                    $('#shipping_address_1').val(data.street);
+                    $(`#${prefix}_city`).val(data.city);
+                    $(`#${prefix}_neighborhood`).val(data.neighborhood);
+                    $(`#${prefix}_address_1`).val(data.street);
                 }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error('Erro ao obter dados da API:', textStatus, errorThrown);
+            },
+            complete: function () {
+                // Esconde o ícone de carregamento
+                $(`#${prefix}_postcode_field`).unblock();
             }
         });
     }
 
-    // Evento de mudança no campo de CEP para cobrança
-    $('#billing_postcode').on('change', function() {
+    // Evento de mudança no campo de CEP
+    $('form.checkout').on('input', '#billing_postcode, #shipping_postcode', function () {
+        var prefix = this.id.split('_')[0];
         var cep = $(this).val().replace(/\D/g, ''); // Remover não dígitos do CEP
         if (cep.length === 8) {
-            autocompleteAddressByCEP(cep);
+            autocompleteAddressByCEP(cep, prefix);
         }
     });
-
-    // Evento de mudança no campo de CEP para envio
-    $('#shipping_postcode').on('change', function() {
-        var cep = $(this).val().replace(/\D/g, ''); // Remover não dígitos do CEP
-        if (cep.length === 8) {
-            autocompleteAddressByCEP(cep);
-        }
-    });
-
 });
